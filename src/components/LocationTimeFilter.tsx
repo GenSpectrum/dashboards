5d25091c83c578dde6c6e0925e80b8fdc4e3b03c
Chaoran Chen <mail@chaoran-chen.de>
2024-05-24 15:24:59 +0200

    feat: properly separate between baseline dataset and variant

--- src/components/LocationTimeFilter.tsx
+++ src/components/LocationTimeFilter.tsx
@@ -1,17 +1,12 @@
 import { useCallback, useEffect, useState } from 'react';
 import { getCurrentRouteInBrowser, type LapisLocation, navigateTo } from '../routing.ts';
 
-export type LocationTimeVariantFilterProps = {
+export type LocationTimeFilterProps = {
     initialLocation: LapisLocation;
-    initialNextcladePangoLineage?: string;
 };
 
-export const LocationTimeVariantFilter = ({
-    initialLocation,
-    initialNextcladePangoLineage = '',
-}: LocationTimeVariantFilterProps) => {
+export const LocationTimeFilter = ({ initialLocation }: LocationTimeFilterProps) => {
     const [location, setLocation] = useState(initialLocation);
-    const [nextcladePangoLineage, setNextcladePangoLineage] = useState(initialNextcladePangoLineage);
 
     useEffect(() => {
         const handleLocationChange = (event: CustomEvent) => {
@@ -22,25 +17,14 @@ export const LocationTimeVariantFilter = ({
             });
         };
 
-        const handleLineageChange = (event: CustomEvent) => {
-            setNextcladePangoLineage(event.detail.nextcladePangoLineage);
-        };
-
         const locationFilter = document.querySelector('gs-location-filter');
         if (locationFilter) {
             locationFilter.addEventListener('gs-location-changed', handleLocationChange);
-        }
-        const lineageFilter = document.querySelector('gs-text-input');
-        if (lineageFilter) {
-            lineageFilter.addEventListener('gs-text-input-changed', handleLineageChange);
         }
 
         return () => {
             if (locationFilter) {
                 locationFilter.removeEventListener('gs-location-changed', handleLocationChange);
-            }
-            if (lineageFilter) {
-                lineageFilter.removeEventListener('gs-text-input-changed', handleLineageChange);
             }
         };
     }, []);
@@ -50,7 +34,6 @@ export const LocationTimeVariantFilter = ({
         navigateTo({
             ...currentRoute,
             ...location,
-            nextcladePangoLineage,
         });
     };
 
@@ -64,14 +47,12 @@ export const LocationTimeVariantFilter = ({
                 initialValue={initialLocationValue}
                 width='100%'
             ></gs-location-filter>
-            <gs-text-input
-                lapisField='nextcladePangoLineage'
-                placeholderText='Pango Lineage'
-                initialValue={initialNextcladePangoLineage}
-                width='100%'
-            ></gs-text-input>
-            <button onClick={() => search()} className='rounded-lg border p-4 hover:bg-amber-200' type='submit'>
-                Search
+            <button
+                onClick={() => search()}
+                className='rounded-lg border bg-white p-4 hover:bg-amber-200'
+                type='submit'
+            >
+                Submit
             </button>
         </div>
     );
---
+++ src/components/VariantFilter.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { changeVariantQuery, getCurrentRouteInBrowser, type LapisLocation, navigateTo } from '../routing.ts';
+
+export type VariantFilterProps = {
+    initialNextcladePangoLineage?: string;
+};
+
+export const VariantFilter = ({ initialNextcladePangoLineage }: VariantFilterProps) => {
+    const [nextcladePangoLineage, setNextcladePangoLineage] = useState(initialNextcladePangoLineage);
+
+    useEffect(() => {
+        const handleLineageChange = (event: CustomEvent) => {
+            setNextcladePangoLineage(event.detail.nextcladePangoLineage);
+        };
+        const lineageFilter = document.querySelector('gs-text-input');
+        if (lineageFilter) {
+            lineageFilter.addEventListener('gs-text-input-changed', handleLineageChange);
+        }
+
+        return () => {
+            if (lineageFilter) {
+                lineageFilter.removeEventListener('gs-text-input-changed', handleLineageChange);
+            }
+        };
+    }, []);
+
+    const search = () => {
+        const currentRoute = getCurrentRouteInBrowser()!;
+        changeVariantQuery(currentRoute, { nextcladePangoLineage });
+        navigateTo(changeVariantQuery(currentRoute, { nextcladePangoLineage }));
+    };
+
+    return (
+        <div className='flex flex-col items-stretch gap-2'>
+            <gs-text-input
+                lapisField='nextcladePangoLineage'
+                placeholderText='Pango Lineage'
+                initialValue={initialNextcladePangoLineage}
+                width='100%'
+            ></gs-text-input>
+            <button
+                onClick={() => search()}
+                className='rounded-lg border bg-white p-4 hover:bg-amber-200'
+                type='submit'
+            >
+                Search
+            </button>
+        </div>
+    );
+};
