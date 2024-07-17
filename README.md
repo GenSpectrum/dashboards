ac518faed52e1c8d8ba2ed5a6daa7ff3a5cb44af
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-07-17 09:35:46 +0200

    feat: make LAPIS urls configurable at runtime #17

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Dashboards
+
+### Getting started
+
+Set up a `.env` file, e.g. by
+
+```bash
+cp .env.example .env
+```
+
+Install dependencies:
+
+```bash
+npm install
+```
+
+Start app in development mode:
+
+```bash
+npm run dev
+```
--- src/routes/View.ts
+++ src/routes/View.ts
@@ -1,4 +1,20 @@
-export type Organism = 'covid' | 'mpox' | 'west-nile' | 'rsv-a' | 'rsv-b';
+export namespace Organisms {
+    export const covid = 'covid';
+    export const mpox = 'mpox';
+    export const westNile = 'west-nile';
+    export const rsvA = 'rsv-a';
+    export const rsvB = 'rsv-b';
+}
+
+export const allOrganisms = [
+    Organisms.covid,
+    Organisms.mpox,
+    Organisms.westNile,
+    Organisms.rsvA,
+    Organisms.rsvB,
+] as const;
+
+export type Organism = (typeof allOrganisms)[number];
 
 export type Route = {
     organism: Organism;
