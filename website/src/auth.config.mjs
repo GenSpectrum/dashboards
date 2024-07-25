6288fd3afea3301b4cb169bc4ca512d5d01febbf
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-07-25 13:08:56 +0200

    feat(website): add GitHub as login provider
    closes #40

--- website/src/auth.config.mjs
+++ website/src/auth.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'auth-astro';
-import { getDashboardsConfig } from './config';
+import { getDashboardsConfig, getSecretsConfig } from './config';
+import GitHub from '@auth/core/providers/github';
 
 const authConfig = getDashboardsConfig().auth;
+const secretsConfig = getSecretsConfig();
 
 export default defineConfig({
     providers: [
+        GitHub({
+            clientId: authConfig.github.clientId,
+            clientSecret: secretsConfig.github.clientSecret,
+        }),
     ],
     secret: crypto.randomUUID(),
 });
--- website/src/config.ts
+++ website/src/config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { allOrganisms, type Organism } from './routes/View.ts';
 
 const DASHBOARDS_CONFIG_FILE = 'dashboards_config.json';
+const SECRETS_FILE = 'secrets.json';
 
 const lapisInstanceConfigSchema = z.object({ lapisUrl: z.string() });
 export type LapisInstanceConfig = z.infer<typeof lapisInstanceConfigSchema>;
@@ -19,11 +20,31 @@ const dashboardsConfigSchema = z
         ),
     )
     .extend({
-        auth: z.object({}),
+        auth: z.object({
+            github: z.object({
+                /**
+                 * The client id of the GitHub App used for authentication.
+                 * https://docs.github.com/en/apps/creating-github-apps/writing-code-for-a-github-app/building-a-login-with-github-button-with-a-github-app#store-the-client-id-and-client-secret
+                 */
+                clientId: z.string(),
+            }),
+        }),
     });
 export type DashboardsConfig = z.infer<typeof dashboardsConfigSchema>;
 
+const secretsConfigSchema = z.object({
+    github: z.object({
+        /**
+         * The client secret of the GitHub App used for authentication.
+         * https://docs.github.com/en/apps/creating-github-apps/writing-code-for-a-github-app/building-a-login-with-github-button-with-a-github-app#store-the-client-id-and-client-secret
+         */
+        clientSecret: z.string(),
+    }),
+});
+export type SecretsConfig = z.infer<typeof secretsConfigSchema>;
+
 let dashboardsConfig: DashboardsConfig | null = null;
+let secretsConfig: SecretsConfig | null = null;
 
 export function getDashboardsConfig(): DashboardsConfig {
     if (dashboardsConfig === null) {
@@ -34,6 +55,13 @@ export function getDashboardsConfig(): DashboardsConfig {
 
 export function getLapisUrl(organism: Organism): string {
     return getDashboardsConfig()[organism].lapisUrl;
+}
+
+export function getSecretsConfig(): SecretsConfig {
+    if (secretsConfig === null) {
+        secretsConfig = readTypedConfigFile(SECRETS_FILE, secretsConfigSchema);
+    }
+    return secretsConfig;
 }
 
 function getConfigDir(): string {
