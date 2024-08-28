c812be18480e88401d40d66b4073f448e6794a0b
Jonas Kellerer (TNG) <jonas.kellerer@tngtech.com>
2024-08-28 09:05:28 +0200

    feat(root): add commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,16 @@
+import {RuleConfigSeverity} from "@commitlint/types";
+
+/**
+ * @type {import('@commitlint/types').UserConfig}
+ */
+const Configuration = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        "scope-empty": [RuleConfigSeverity.Disabled],
+        "body-max-line-length": [RuleConfigSeverity.Disabled],
+    },
+    defaultIgnores: false,
+    ignores: [(c) => c.startsWith('Merge'),],
+};
+
+export default Configuration;
