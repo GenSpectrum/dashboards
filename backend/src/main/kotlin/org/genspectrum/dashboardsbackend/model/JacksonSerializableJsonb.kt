6a137359affb028d046759804ee86317cf96e163
Jonas Kellerer (TNG) <jonas.kellerer@tngtech.com>
2024-08-30 08:51:15 +0200

    feat(backend): use database to store subscriptions #74

---
+++ backend/src/main/kotlin/org/genspectrum/dashboardsbackend/model/JacksonSerializableJsonb.kt
@@ -0,0 +1,18 @@
+package org.genspectrum.dashboardsbackend.model
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.json.jsonb
+
+val jacksonObjectMapper: ObjectMapper = jacksonObjectMapper().findAndRegisterModules()
+inline fun <reified T : Any> Table.jacksonSerializableJsonb(columnName: String) = jsonb<T>(
+    columnName,
+    ::serialize,
+    ::deserialize,
+)
+
+inline fun <reified T : Any> serialize(value: T): String = jacksonObjectMapper.writeValueAsString(value)
+
+inline fun <reified T : Any> deserialize(value: String): T = jacksonObjectMapper.readValue(value)
