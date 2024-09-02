0860c3bb36493622cd7743629d943c1d27f346c7
Jonas Kellerer (TNG) <jonas.kellerer@tngtech.com>
2024-09-02 12:39:17 +0200

    feat(website): add proxy for backend to website

--- website/src/auth.config.mjs
+++ website/src/auth.config.mjs
@@ -6,6 +6,15 @@ const authConfig = getDashboardsConfig().auth;
 const secretsConfig = getSecretsConfig();
 
 export default defineConfig({
+    callbacks: {
+        session: async ({ session, token }) => {
+            if (session?.user) {
+                session.user.id = token.sub;
+            }
+
+            return session;
+        },
+    },
     providers: [
         GitHub({
             clientId: authConfig.github.clientId,
