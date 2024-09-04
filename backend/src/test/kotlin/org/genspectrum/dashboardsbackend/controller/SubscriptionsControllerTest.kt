35f4cca5746d4aa30c2d1b8c582b7d2b4f366d9d
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-09-04 15:48:40 +0200

    feat(backend): implement DELETE /subscriptions/{id}
    resolves #78

--- backend/src/test/kotlin/org/genspectrum/dashboardsbackend/controller/SubscriptionsControllerTest.kt
+++ backend/src/test/kotlin/org/genspectrum/dashboardsbackend/controller/SubscriptionsControllerTest.kt
@@ -4,10 +4,13 @@ import org.genspectrum.dashboardsbackend.subscriptions.CountTrigger
 import org.genspectrum.dashboardsbackend.subscriptions.DateWindow
 import org.genspectrum.dashboardsbackend.subscriptions.EvaluationInterval
 import org.genspectrum.dashboardsbackend.subscriptions.Organism
+import org.genspectrum.dashboardsbackend.subscriptions.Subscription
 import org.genspectrum.dashboardsbackend.subscriptions.SubscriptionRequest
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.hasItem
+import org.hamcrest.Matchers.hasProperty
+import org.hamcrest.Matchers.not
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -47,7 +50,7 @@ class SubscriptionsControllerTest(
     }
 
     @Test
-    fun `WHEN getting subscriptions with invalid UUID THEN returns 404`() {
+    fun `WHEN getting subscriptions with invalid UUID THEN returns 400`() {
         subscriptionsClient.getSubscriptionRaw("this-is-not-a-uuid")
             .andExpect(status().isBadRequest)
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -69,5 +72,37 @@ class SubscriptionsControllerTest(
         val subscription = subscriptionsClient.getSubscription(createdSubscription.id)
 
         assertThat(subscription, equalTo(createdSubscription))
+    }
+
+    @Test
+    fun `WHEN I delete a subscription that does not exist THEN returns 404`() {
+        subscriptionsClient.deleteSubscriptionRaw("00000000-0000-0000-0000-000000000000")
+            .andExpect(status().isNotFound)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.detail").value("Subscription 00000000-0000-0000-0000-000000000000 not found"))
+    }
+
+    @Test
+    fun `WHEN I delete a subscription with invalid UUID THEN return 400`() {
+        subscriptionsClient.deleteSubscriptionRaw("this-is-not-a-uuid")
+            .andExpect(status().isBadRequest)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.detail").value("Invalid UUID this-is-not-a-uuid"))
+    }
+
+    @Test
+    fun `GIVEN subscription exists WHEN I delete it THEN it does not exist anymore`() {
+        val createdSubscription = subscriptionsClient.postSubscription(dummySubscriptionRequest)
+
+        subscriptionsClient.deleteSubscriptionRaw(createdSubscription.id)
+            .andExpect(status().isNoContent)
+
+        subscriptionsClient.getSubscriptionRaw(createdSubscription.id)
+            .andExpect(status().isNotFound)
+
+        assertThat(
+            subscriptionsClient.getSubscriptions(),
+            not(hasItem(hasProperty<Subscription>("id", equalTo(createdSubscription.id)))),
+        )
     }
 }
