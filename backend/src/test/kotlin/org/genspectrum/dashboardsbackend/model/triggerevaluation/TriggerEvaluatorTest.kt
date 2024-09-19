5c2c0105514e840aed246b2ffc7a0534de383ed1
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-09-19 17:16:00 +0200

    test(backend): refactor test and make it execute for all organisms

--- backend/src/test/kotlin/org/genspectrum/dashboardsbackend/model/triggerevaluation/TriggerEvaluatorTest.kt
+++ backend/src/test/kotlin/org/genspectrum/dashboardsbackend/model/triggerevaluation/TriggerEvaluatorTest.kt
@@ -8,9 +8,12 @@ import org.genspectrum.dashboardsbackend.api.Trigger
 import org.genspectrum.dashboardsbackend.api.TriggerEvaluationResult
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import org.mockserver.client.MockServerClient
+import org.mockserver.model.HttpRequest
 import org.mockserver.model.HttpRequest.request
+import org.mockserver.model.HttpResponse
 import org.mockserver.model.HttpResponse.response
 import org.mockserver.model.MediaType
 import org.mockserver.springtest.MockServerTest
@@ -22,9 +25,9 @@ import org.springframework.boot.test.context.SpringBootTest
         "dashboards.covid.lapisUrl=http://localhost:\${mockServerPort}/covid",
         "dashboards.h5n1.lapisUrl=http://localhost:\${mockServerPort}/h5n1",
         "dashboards.mpox.lapisUrl=http://localhost:\${mockServerPort}/mpox",
-        "dashboards.westNile.lapisUrl=http://localhost:\${mockServerPort}/westNile",
-        "dashboards.rsvA.lapisUrl=http://localhost:\${mockServerPort}/rsvA",
-        "dashboards.rsvB.lapisUrl=http://localhost:\${mockServerPort}/rsvB",
+        "dashboards.westNile.lapisUrl=http://localhost:\${mockServerPort}/west_nile",
+        "dashboards.rsvA.lapisUrl=http://localhost:\${mockServerPort}/rsv_a",
+        "dashboards.rsvB.lapisUrl=http://localhost:\${mockServerPort}/rsv_b",
     ],
 )
 @MockServerTest
@@ -33,44 +36,20 @@ class TriggerEvaluatorTest(
 ) {
     private lateinit var mockServerClient: MockServerClient
 
-    @Test
-    fun `GIVEN ___`() { // TODO test title
+    @ParameterizedTest
+    @MethodSource("getOrganisms")
+    fun `GIVEN lapis returns count above count trigger threshold THEN returns condition met`(organism: Organism) {
         mockServerClient
             .`when`(
-                request()
-                    .withMethod("POST")
-                    .withPath("/westNile/sample/aggregated")
-                    .withContentType(MediaType.APPLICATION_JSON)
-                    .withBody(
-                        """{"country":"Germany","division":"Berlin"}""".trimIndent(),
-                    ),
+                requestingAggregatedDataWith(
+                    organism = organism,
+                    body = """{"country":"Germany","division":"Berlin"}""".trimIndent(),
+                ),
             )
-            .respond(
-                response()
-                    .withStatusCode(200)
-                    .withBody(
-                        """
-                            {
-                                "data": [
-                                    {
-                                        "count": 101
-                                    }
-                                ],
-                                "info": {
-                                    "dataVersion": "a data version"
-                                }
-                            }
-                        """.trimIndent(),
-                    ),
-            )
+            .respond(withSuccessResponse(count = 101))
 
-        val subscription = Subscription(
-            id = "id",
-            name = "test",
-            interval = EvaluationInterval.WEEKLY,
-            active = true,
-            organism = Organism.WEST_NILE,
-            dateWindow = DateWindow.LAST_6_MONTHS,
+        val subscription = makeSubscription(
+            organism = organism,
             trigger = Trigger.CountTrigger(
                 count = 100,
                 filter = mapOf(
@@ -83,5 +62,45 @@ class TriggerEvaluatorTest(
         val result = underTest.evaluate(subscription)
 
         assertThat(result, `is`(TriggerEvaluationResult.ConditionMet))
+    }
+
+    private fun requestingAggregatedDataWith(organism: Organism, body: String): HttpRequest? = request()
+        .withMethod("POST")
+        .withContentType(MediaType.APPLICATION_JSON)
+        .withPath(getAggregatedRoute(organism))
+        .withBody(body)
+
+    private fun withSuccessResponse(count: Int): HttpResponse? = response()
+        .withStatusCode(200)
+        .withBody(
+            """
+                {
+                    "data": [
+                        {
+                            "count": $count
+                        }
+                    ],
+                    "info": {
+                        "dataVersion": "a data version"
+                    }
+                }
+            """.trimIndent(),
+        )
+
+    private fun makeSubscription(organism: Organism, trigger: Trigger.CountTrigger) = Subscription(
+        id = "id",
+        name = "test",
+        interval = EvaluationInterval.WEEKLY,
+        active = true,
+        organism = organism,
+        dateWindow = DateWindow.LAST_6_MONTHS,
+        trigger = trigger,
+    )
+
+    fun getAggregatedRoute(organism: Organism) = "/${organism.name.lowercase()}/sample/aggregated"
+
+    companion object {
+        @JvmStatic
+        fun getOrganisms() = Organism.entries.toList()
     }
 }
