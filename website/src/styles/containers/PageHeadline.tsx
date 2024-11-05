1ee38e0b4e9a1e264dba1b1efc98e4b4c7330240
Jonas Zarzalis (TNG) <jonas.zarzalis@tngtech.com>
2024-11-05 10:41:43 +0100

    feat(website): add breadcrumbs to subscription pages
    Resolves #221

--- website/src/styles/containers/PageHeadline.tsx
+++ website/src/styles/containers/PageHeadline.tsx
@@ -1,5 +1,5 @@
 import { type PropsWithChildren } from 'react';
 
 export function PageHeadline({ children }: PropsWithChildren) {
-    return <h1 className='mb-4 mt-6 text-xl font-bold'>{children}</h1>;
+    return <h1 className='mb-4 text-xl font-bold'>{children}</h1>;
 }
