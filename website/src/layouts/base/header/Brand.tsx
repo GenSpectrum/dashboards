d74bb3c524b9537c2b1d95796040964378457486
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-11-14 14:24:51 +0100

    feat(website): add favicon to header

--- website/src/layouts/base/header/Brand.tsx
+++ website/src/layouts/base/header/Brand.tsx
@@ -1,7 +1,10 @@
 export function Brand({ name }: { name: string }) {
     return (
-        <a className='ml-2 font-bold' href='/'>
-            {name}
-        </a>
+        <div>
+            <a href='/' className='my-8 flex items-center'>
+                <img className='ml-4 mr-2' src='/favicon.png' alt='icon' />
+                <div className='ml-2 font-bold'>{name}</div>
+            </a>
+        </div>
     );
 }
