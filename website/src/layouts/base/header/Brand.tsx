2beafd8d494f8be05df160c1dba866fac799a86b
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-11-18 23:34:53 +0100

    other colors

--- website/src/layouts/base/header/Brand.tsx
+++ website/src/layouts/base/header/Brand.tsx
@@ -2,7 +2,7 @@ export function Brand() {
     return (
         <div className='my-8 flex items-center'>
             <a href='/'>
-                <img className='ml-4 mr-2' src='/virus-outline-colorful.svg' alt='icon' width='20' height='20' />
+                <img className='ml-4 mr-2' src='/virus-outline-colorful-anya.svg' alt='icon' width='20' height='20' />
             </a>
             <a href='/' className='ml-2'>
             Gen<span className='text-cyan font-bold'>S</span>
