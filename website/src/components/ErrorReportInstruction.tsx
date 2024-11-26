de67e381154fa235e1983e67c4f74e15693e2ce6
Fabian Engelniederhammer <92720311+fengelniederhammer@users.noreply.github.com>
2024-11-26 13:11:57 +0100

    feat(website): add button to copy error information to clipboard (#347)
    resolves #264

--- website/src/components/ErrorReportInstruction.tsx
+++ website/src/components/ErrorReportInstruction.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 export function ErrorReportToastModal({ errorId }: { errorId: string }) {
     const modalRef = useRef<HTMLDialogElement>(null);
@@ -32,22 +32,55 @@ export function ErrorReportToastModal({ errorId }: { errorId: string }) {
 }
 
 export function ErrorReportInstruction({ errorId, currentUrl }: { errorId: string; currentUrl: string }) {
+    const errorInfoRef = useRef<HTMLUListElement>(null);
+
     return (
         <>
             <h3 className='mb-4 text-lg font-bold'>Help us fix the issue</h3>
-            <p>
+            <div>
                 Please create an issue on{' '}
                 <a className='text-primary underline' href='https://github.com/GenSpectrum/dashboards'>
                     GitHub
                 </a>
                 . Describe what you did to trigger the error and include the following information so that we can track
-                down the issue:
-                <ul className='ml-6 mt-2 list-disc'>
-                    <li>Error ID: {errorId}</li>
-                    <li>Time: {new Date().toISOString()}</li>
-                    <li>Current URL: {currentUrl}</li>
-                </ul>
-            </p>
+                down the issue.
+                <div className='mt-4 flex rounded-lg bg-gray-100 p-4'>
+                    <ul ref={errorInfoRef} className='ml-6 flex-grow list-inside list-disc overflow-scroll'>
+                        <li>Error ID: {errorId}</li>
+                        <li>Time: {new Date().toISOString()}</li>
+                        <li>Current URL: {currentUrl}</li>
+                    </ul>
+                    <CopyToClipboardButton getTextToCopy={() => errorInfoRef.current?.innerText} />
+                </div>
+            </div>
         </>
+    );
+}
+
+function CopyToClipboardButton({ getTextToCopy }: { getTextToCopy: () => string | undefined }) {
+    const [copiedRecently, setCopiedRecently] = useState(false);
+
+    return (
+        <div
+            className='tooltip tooltip-left mx-4 my-auto'
+            data-tip={`${copiedRecently ? 'Copied' : 'Copy'} error information to clipboard`}
+        >
+            {copiedRecently ? (
+                <span className='iconify text-2xl text-green mdi--tick' />
+            ) : (
+                <button
+                    className='iconify text-xl mdi--content-copy'
+                    onClick={async () => {
+                        const text = getTextToCopy();
+                        if (text === undefined) {
+                            return;
+                        }
+                        await navigator.clipboard.writeText(text);
+                        setCopiedRecently(true);
+                        setTimeout(() => setCopiedRecently(false), 3000);
+                    }}
+                />
+            )}
+        </div>
     );
 }
