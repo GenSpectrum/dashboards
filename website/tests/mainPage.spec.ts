5ceaffd543d80b2f4435685d968b988e64a6c73e
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-11-27 14:33:44 +0100

    test(website): add e2e test setup with playwright and some basic tests
    resolves #348

---
+++ website/tests/mainPage.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+const organisms = ['SARS-CoV-2', 'Influenza A/H5N1', 'West Nile', 'RSV-A', 'RSV-B'];
+const views = [
+    {
+        linkName: 'Analyze a single variant',
+        title: 'Single variant',
+        expectedHeadline: 'Prevalence over time',
+    },
+    {
+        linkName: 'Compare variants side-by-side',
+        title: 'Compare side-by-side',
+        expectedHeadline: 'Prevalence over time',
+    },
+    { linkName: 'Sequencing efforts', title: 'Sequencing efforts', expectedHeadline: 'Number sequences' },
+];
+
+test.describe('Main page', () => {
+    test('shows heading', async ({ page }) => {
+        await page.goto('/');
+
+        await expect(page).toHaveTitle('GenSpectrum Dashboards');
+        await expect(page.getByRole('heading', { name: 'GenSpectrum' }).locator('div')).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Inspect pathogen genomic data' })).toBeVisible();
+    });
+
+    test(`should have a main organism navigation that links to the organism views`, async ({ page }) => {
+        for (const organism of organisms) {
+            for (const { linkName, title, expectedHeadline } of views) {
+                await page.goto('/');
+                await page.getByRole('heading', { name: organism }).locator('..').getByText(linkName).click();
+                await expect(page).toHaveTitle(`${title} | ${organism} | GenSpectrum`);
+                await expect(page.getByRole('heading', { name: expectedHeadline }).first()).toBeVisible();
+            }
+        }
+    });
+});
