d63f421a472861797604e5b705c557bb863e88a4
Jonas Zarzalis (TNG) <jonas.zarzalis@tngtech.com>
2024-12-06 15:49:53 +0100

    feat(website): add fallback element for filters
    Resolves #363

--- website/src/components/pageStateSelectors/SequencingEffortsPageStateSelector.tsx
+++ website/src/components/pageStateSelectors/SequencingEffortsPageStateSelector.tsx
@@ -35,7 +35,7 @@ export function SequencingEffortsPageStateSelector({
     );
 
     return (
-        <div className='flex flex-col gap-6 bg-gray-50 p-2'>
+        <div className='flex flex-col gap-6'>
             <div>
                 <SelectorHeadline>Filter dataset</SelectorHeadline>
                 <BaselineSelector
