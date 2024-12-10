cdb7272e3105fc94fcda93b44b1ae7f31a9eafc5
Jonas Zarzalis (TNG) <jonas.zarzalis@tngtech.com>
2024-12-10 15:01:51 +0100

    feat(website): bring back mpox
    Resolves: #395

---
+++ website/src/views/mpox.ts
@@ -0,0 +1,155 @@
+import { type DateRangeOption, dateRangeOptionPresets } from '@genspectrum/dashboard-components/util';
+
+import { type CompareSideBySideData, type DatasetAndVariantData, type Id } from './View.ts';
+import { type OrganismsConfig } from '../config.ts';
+import {
+    BaseView,
+    GenericCompareToBaselineView,
+    GenericCompareVariantsView,
+    GenericSequencingEffortsView,
+    GenericSingleVariantView,
+} from './BaseView.ts';
+import type { SingleVariantConstants } from './OrganismConstants.ts';
+import { compareSideBySideViewConstants } from './ViewConstants.ts';
+import type { LineageFilterConfig } from '../components/pageStateSelectors/LineageFilterInput.tsx';
+import { organismConfig, Organisms } from '../types/Organism.ts';
+import type { DataOrigin } from '../types/dataOrigins.ts';
+import { GenericCompareSideBySideStateHandler } from './pageStateHandlers/CompareSideBySidePageStateHandler.ts';
+
+class MpoxConstants implements SingleVariantConstants {
+    public readonly organism = Organisms.mpox;
+    public readonly earliestDate = '1960-01-01';
+    public readonly defaultDateRange = dateRangeOptionPresets.lastYear;
+    public readonly dateRangeOptions: DateRangeOption[] = [
+        dateRangeOptionPresets.lastMonth,
+        dateRangeOptionPresets.last2Months,
+        dateRangeOptionPresets.last3Months,
+        dateRangeOptionPresets.last6Months,
+        dateRangeOptionPresets.lastYear,
+        { label: '2024', dateFrom: '2024-01-01' },
+        { label: '2023', dateFrom: '2023-01-01', dateTo: '2023-12-31' },
+        { label: '2022', dateFrom: '2022-01-01', dateTo: '2022-12-31' },
+        { label: '2021', dateFrom: '2021-01-01', dateTo: '2021-12-31' },
+        { label: 'Since 2021', dateFrom: '2021-01-01' },
+        { label: 'Before 2021', dateFrom: this.earliestDate, dateTo: '2020-12-31' },
+        { label: 'Since 2017', dateFrom: '2017-01-01' },
+        { label: '2017-2020', dateFrom: '2017-01-01', dateTo: '2020-12-31' },
+        { label: 'Before 2017', dateFrom: this.earliestDate, dateTo: '2016-12-31' },
+    ];
+    public readonly mainDateField: string;
+    public readonly locationFields: string[];
+    public readonly lineageFilters: LineageFilterConfig[] = [
+        {
+            lapisField: 'lineage',
+            placeholderText: 'Lineage',
+            filterType: 'text' as const,
+            initialValue: undefined,
+        },
+        {
+            lapisField: 'clade',
+            placeholderText: 'Clade',
+            filterType: 'text' as const,
+            initialValue: undefined,
+        },
+    ];
+    public readonly hostField: string;
+    public readonly authorsField: string | undefined;
+    public readonly authorAffiliationsField: string | undefined;
+    public readonly additionalSequencingEffortsFields = [
+        { label: 'Collection device', fieldName: 'collectionDevice' },
+        { label: 'Collection method', fieldName: 'collectionMethod' },
+        { label: 'Purpose of sampling', fieldName: 'purposeOfSampling' },
+        { label: 'Sample type', fieldName: 'sampleType' },
+        { label: 'Amplicon PCR primer scheme', fieldName: 'ampliconPcrPrimerScheme' },
+        { label: 'Sequencing protocol', fieldName: 'sequencingProtocol' },
+    ];
+    public readonly additionalFilters: Record<string, string> | undefined;
+    public readonly dataOrigins: DataOrigin[] = ['pathoplexus'];
+
+    constructor(organismsConfig: OrganismsConfig) {
+        this.mainDateField = organismsConfig.mpox.lapis.mainDateField;
+        this.locationFields = organismsConfig.mpox.lapis.locationFields;
+        this.hostField = organismsConfig.mpox.lapis.hostField;
+        this.authorsField = organismsConfig.mpox.lapis.authorsField;
+        this.authorAffiliationsField = organismsConfig.mpox.lapis.authorAffiliationsField;
+        this.additionalFilters = organismsConfig.mpox.lapis.additionalFilters;
+    }
+}
+
+export class MpoxAnalyzeSingleVariantView extends GenericSingleVariantView<MpoxConstants> {
+    constructor(organismsConfig: OrganismsConfig) {
+        super(new MpoxConstants(organismsConfig));
+    }
+}
+
+export class MpoxCompareSideBySideView extends BaseView<
+    CompareSideBySideData,
+    MpoxConstants,
+    GenericCompareSideBySideStateHandler
+> {
+    constructor(organismsConfig: OrganismsConfig) {
+        const constants = new MpoxConstants(organismsConfig);
+        const defaultPageState = {
+            filters: new Map<Id, DatasetAndVariantData>([
+                [
+                    0,
+                    {
+                        datasetFilter: {
+                            location: {},
+                            dateRange: constants.defaultDateRange,
+                        },
+                        variantFilter: {
+                            lineages: {
+                                lineage: 'F.1',
+                            },
+                            mutations: {},
+                        },
+                    },
+                ],
+                [
+                    1,
+                    {
+                        datasetFilter: {
+                            location: {},
+                            dateRange: constants.defaultDateRange,
+                        },
+                        variantFilter: {
+                            lineages: {
+                                lineage: 'F.2',
+                            },
+                            mutations: {},
+                        },
+                    },
+                ],
+            ]),
+        };
+
+        super(
+            constants,
+            new GenericCompareSideBySideStateHandler(
+                constants,
+                defaultPageState,
+                organismConfig[constants.organism].pathFragment,
+            ),
+            compareSideBySideViewConstants,
+        );
+    }
+}
+
+export class MpoxSequencingEffortsView extends GenericSequencingEffortsView<MpoxConstants> {
+    constructor(organismsConfig: OrganismsConfig) {
+        super(new MpoxConstants(organismsConfig));
+    }
+}
+
+export class MpoxCompareVariantsView extends GenericCompareVariantsView<MpoxConstants> {
+    constructor(organismsConfig: OrganismsConfig) {
+        super(new MpoxConstants(organismsConfig));
+    }
+}
+
+export class MpoxCompareToBaselineView extends GenericCompareToBaselineView<MpoxConstants> {
+    constructor(organismsConfig: OrganismsConfig) {
+        super(new MpoxConstants(organismsConfig));
+    }
+}
--- website/src/views/routing.ts
+++ website/src/views/routing.ts
@@ -14,6 +14,13 @@ import {
     H5n1CompareVariantsView,
     H5n1SequencingEffortsView,
 } from './h5n1.ts';
+import {
+    MpoxAnalyzeSingleVariantView,
+    MpoxCompareSideBySideView,
+    MpoxCompareToBaselineView,
+    MpoxCompareVariantsView,
+    MpoxSequencingEffortsView,
+} from './mpox.ts';
 import type { PageStateHandler } from './pageStateHandlers/PageStateHandler.ts';
 import {
     RsvAAnalyzeSingleVariantView,
@@ -100,6 +107,13 @@ export class Routing {
                 [compareVariantsViewKey]: new WestNileCompareVariantsView(organismsConfig),
                 [compareToBaselineViewKey]: new WestNileCompareToBaselineView(organismsConfig),
             },
+            [Organisms.mpox]: {
+                [singleVariantViewKey]: new MpoxAnalyzeSingleVariantView(organismsConfig),
+                [compareSideBySideViewKey]: new MpoxCompareSideBySideView(organismsConfig),
+                [sequencingEffortsViewKey]: new MpoxSequencingEffortsView(organismsConfig),
+                [compareVariantsViewKey]: new MpoxCompareVariantsView(organismsConfig),
+                [compareToBaselineViewKey]: new MpoxCompareToBaselineView(organismsConfig),
+            },
         } as const;
 
         this.externalPages = this.initializeExternalPages(organismsConfig);
@@ -117,6 +131,8 @@ export class Routing {
                 return Object.values(this.views[Organisms.rsvB]);
             case Organisms.westNile:
                 return Object.values(this.views[Organisms.westNile]);
+            case Organisms.mpox:
+                return Object.values(this.views[Organisms.mpox]);
         }
     }
 
