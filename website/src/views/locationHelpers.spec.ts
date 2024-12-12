aab26ff18d0dae1c76832255ca858b8f6cc31e24
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-12-12 16:21:32 +0100

    feat(website): add map of geographic distribution to sequencing efforts pages
    resolves #396

---
+++ website/src/views/locationHelpers.spec.ts
@@ -0,0 +1,95 @@
+import { describe, expect, test } from 'vitest';
+
+import { getLocationDisplayConfig, getLocationSubdivision } from './locationHelpers.ts';
+
+const continentField = 'myContinentField';
+const countryField = 'myCountryField';
+const divisionField = 'myDivisionField';
+const locationFields = [continentField, countryField, divisionField];
+
+describe('getLocationDisplayConfig', () => {
+    test('should return World with country field if no location is set', () => {
+        const locationFilter = {};
+
+        const actual = getLocationDisplayConfig(locationFields, locationFilter);
+
+        const expected = { label: 'World', mapName: 'World', locationField: countryField };
+        expect(actual).toEqual(expected);
+    });
+
+    test('should return the correct location display config for a continent', () => {
+        const locationFilter = {
+            [continentField]: 'myContinentValue',
+        };
+
+        const actual = getLocationDisplayConfig(locationFields, locationFilter);
+
+        const expected = {
+            label: 'Country',
+            mapName: 'myContinentValue',
+            locationField: countryField,
+        };
+        expect(actual).toEqual(expected);
+    });
+
+    test('should return the correct location display config for a country', () => {
+        const locationFilter = {
+            [continentField]: 'myContinentValue',
+            [countryField]: 'myCountryValue',
+        };
+
+        const actual = getLocationDisplayConfig(locationFields, locationFilter);
+
+        const expected = {
+            label: 'Geographic sub-divisions',
+            mapName: 'myCountryValue',
+            locationField: divisionField,
+        };
+        expect(actual).toEqual(expected);
+    });
+});
+
+describe('getLocationSubdivision', () => {
+    test('should return empty label and undefined field if there is only one location field', () => {
+        const locationFields = ['myField'];
+        const locationFilter = {
+            myField: 'myValue',
+        };
+
+        const actual = getLocationSubdivision(locationFields, locationFilter);
+
+        const expected = { label: '', field: undefined };
+        expect(actual).toEqual(expected);
+    });
+
+    test('should return the correct location subdivision for a continent', () => {
+        const locationFilter = {
+            [continentField]: 'myContinentValue',
+        };
+
+        const actual = getLocationSubdivision(locationFields, locationFilter);
+
+        const expected = {
+            label: 'Country',
+            field: countryField,
+            locationValue: 'myContinentValue',
+        };
+        expect(actual).toEqual(expected);
+    });
+
+    test('should return the correct location subdivision for a country', () => {
+        const locationFilter = {
+            [continentField]: 'myContinentValue',
+            [countryField]: 'myCountryValue',
+        };
+
+        const actual = getLocationSubdivision(locationFields, locationFilter);
+
+        const expected = {
+            label: 'Geographic sub-divisions',
+            field: divisionField,
+            locationValue: 'myCountryValue',
+        };
+        expect(actual).toEqual(expected);
+    });
+});
