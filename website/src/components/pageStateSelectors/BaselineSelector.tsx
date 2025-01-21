1833ab935d11d9f5c4bfbe0cc0738439a3e24c61
Jonas Zarzalis (TNG) <jonas.zarzalis@tngtech.com>
2025-01-21 09:20:02 +0100

    wip: add labels

--- website/src/components/pageStateSelectors/BaselineSelector.tsx
+++ website/src/components/pageStateSelectors/BaselineSelector.tsx
@@ -4,7 +4,7 @@ import type { LapisLocation } from '../../views/helpers.ts';
 import { GsDateRangeSelector } from '../genspectrum/GsDateRangeSelector.tsx';
 import { GsLocationFilter } from '../genspectrum/GsLocationFilter.tsx';
 import { GsTextInput } from '../genspectrum/GsTextInput.tsx';
-import {useState} from "react";
+import { useState } from 'react';
 
 export type LocationFilterConfig = {
     locationFields: string[];
@@ -30,11 +30,12 @@ export function BaselineSelector({
     onDateRangeChange: (dateRange: DateRangeOption) => void;
     dateRangeFilterConfig: DateRangeFilterConfig;
 }) {
-    const [openLarge, setOpenLarge] = useState(false)
+    const [openLarge, setOpenLarge] = useState(false);
 
     return (
         <div>
             <div className={`flex flex-col gap-2 overflow-y-scroll ${openLarge ? 'max-h-[100vh]' : 'max-h-[35vh]'}`}>
+                <div>Sampling location</div>
                 <GsLocationFilter
                     fields={locationFilterConfig.locationFields}
                     onLocationChange={onLocationChange}
@@ -44,6 +45,7 @@ export function BaselineSelector({
                         .join(' / ')}
                     placeholderText={locationFilterConfig.placeholderText}
                 ></GsLocationFilter>
+                <div>Date</div>
                 <GsDateRangeSelector
                     lapisDateField={dateRangeFilterConfig.dateColumn}
                     onDateRangeChange={onDateRangeChange}
@@ -51,7 +53,9 @@ export function BaselineSelector({
                     initialValue={dateRangeFilterConfig.initialDateRange}
                     dateRangeOptions={dateRangeFilterConfig.dateRangeOptions}
                 ></GsDateRangeSelector>
+                <div>Age</div>
                 <GsTextInput lapisField={'age'} placeholderText={'Age'}></GsTextInput>
+                <div>Some other date</div>
                 <GsDateRangeSelector
                     lapisDateField={dateRangeFilterConfig.dateColumn}
                     onDateRangeChange={onDateRangeChange}
@@ -59,12 +63,19 @@ export function BaselineSelector({
                     initialValue={dateRangeFilterConfig.initialDateRange}
                     dateRangeOptions={dateRangeFilterConfig.dateRangeOptions}
                 ></GsDateRangeSelector>
+                <div>Host</div>
                 <GsTextInput lapisField={'host'} placeholderText={'Host'}></GsTextInput>
+                <div>Submitting lab</div>
                 <GsTextInput lapisField={'submittingLab'} placeholderText={'Submitting lab'}></GsTextInput>
+                <div>Sampling strategy</div>
                 <GsTextInput lapisField={'samplingStrategy'} placeholderText={'Sampling strategy'}></GsTextInput>
             </div>
-            <button className='mb-4' onClick={() => setOpenLarge(!openLarge)}>
-                {openLarge? "Arrow up" : "Arrow down"}
+            <button className='btn btn-outline btn-xs mb-4 mt-2 w-full' onClick={() => setOpenLarge(!openLarge)}>
+                {openLarge ? (
+                    <div className='iconify mdi--keyboard-arrow-up'></div>
+                ) : (
+                    <div className='iconify mdi--keyboard-arrow-down'></div>
+                )}
             </button>
         </div>
     );
