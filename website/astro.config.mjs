6c2dd26667b8f910375d20dea41344f09d54251c
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-02-09 13:59:52 +0100

    add playform compress

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,9 +4,11 @@ import react from '@astrojs/react';
 import node from '@astrojs/node';
 import auth from 'auth-astro';
 
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react(), auth({ configFile: './src/auth.config' })],
+    integrations: [tailwind(), react(), auth({ configFile: './src/auth.config' }), playformCompress()],
     output: 'server',
     adapter: node({
         mode: 'standalone',
