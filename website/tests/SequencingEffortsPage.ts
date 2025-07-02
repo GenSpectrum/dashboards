9b6da4af01ccbab527787e6298e8fc5e7247125a
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2025-07-02 10:45:10 +0200

    fixup! fix(website): don't apply own field to lapis filter when filtering values of input components

--- website/tests/SequencingEffortsPage.ts
+++ website/tests/SequencingEffortsPage.ts
@@ -4,22 +4,23 @@ import { ViewPage } from './ViewPage.ts';
 import { type Organism, paths } from '../src/types/Organism.ts';
 
 export class SequencingEffortsPage extends ViewPage {
+    public locationField = this.page.getByPlaceholder('Sampling location');
+
     public async goto(organism: Organism) {
         await this.page.goto(`${paths[organism].basePath}/sequencing-efforts`);
     }
 
     public async selectLocation(location: string) {
-        const locationField = this.page.getByPlaceholder('Sampling location');
         const matchingOption = this.page
             .getByRole('option', { name: new RegExp(`^${location}`, 'i'), exact: true })
             .first();
 
         await expect(async () => {
-            await locationField.click();
+            await this.locationField.click();
             await expect(matchingOption).toBeVisible();
         }).toPass();
 
-        await locationField.fill(location);
+        await this.locationField.fill(location);
         await matchingOption.click();
     }
 }
