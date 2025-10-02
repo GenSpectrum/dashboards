d3b57547545039f9ed1fdf6769d85b90cdc50a7a
Felix Hennig <mail@felixhennig.com>
2025-10-02 17:20:57 +0200

    test WIP

--- website/src/lapis/getMutations.spec.ts
+++ website/src/lapis/getMutations.spec.ts
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, test } from 'vitest';
 
-import { getMutations } from './getMutations.ts';
+import { getMutations, getMutationsForVariant } from './getMutations.ts';
 import { DUMMY_LAPIS_URL } from '../../routeMocker.ts';
 import { astroApiRouteMocker, lapisRouteMocker } from '../../vitest.setup.ts';
 
@@ -25,6 +25,22 @@ describe('getMutations', () => {
         expect(result).toEqual(['A123C']);
     });
 
+    test('should return filtered mutations without lineage', async () => {
+        lapisRouteMocker.mockPostNucleotideMutations(
+            { minProportion: 0 },
+            {
+                data: [
+                    { mutation: 'A123C', count: 58 },
+                    { mutation: 'G234T', count: 223 },
+                ],
+            },
+        );
+
+        const result = await getMutations(DUMMY_LAPIS_URL, 'nucleotide', undefined, 0, 3);
+
+        expect(result).toEqual(['A123C', 'G234T']);
+    });
+
     test('should throw on failed request', async () => {
         lapisRouteMocker.mockPostNucleotideMutations(
             { minProportion: 0.1 },
@@ -46,3 +62,43 @@ describe('getMutations', () => {
         );
     });
 });
+
+describe('getMutationsForVariant', () => {
+    beforeEach(() => {
+        astroApiRouteMocker.mockLog();
+    });
+
+    test('foo', async () => {
+        lapisRouteMocker.mockPostAggregated({pangoLineage: 'FOO'}, { data: [{ count: 20 }] });
+        lapisRouteMocker.mockPostNucleotideMutations(
+            { minProportion: 0.1, pangoLineage: 'FOO' },
+            {
+                data: [
+                    { mutation: 'A1C', count: 20 },
+                    { mutation: 'A2C', count: 20 },
+                    { mutation: 'A3C', count: 2 },
+                ],
+            },
+        );
+        lapisRouteMocker.mockPostNucleotideMutations(
+            { minProportion: 0 },
+            {
+                data: [
+                    { mutation: 'A1C', count: 40 },
+                    { mutation: 'A2C', count: 20 },
+                    { mutation: 'A3C', count: 2 },
+                ],
+            },
+        );
+
+        // TODO - the test is failing. I think, it is because the resolver is just matching the URL
+        // and when the body doesn't match, we get a 400 error. it'd be great if instead, the resolver
+        // would return undefined, and let the next matcher try.
+        // If I'm reading this right, we generally can't have multiple responsesn for the same endpoint
+        // with different params.
+
+        const result = await getMutationsForVariant(DUMMY_LAPIS_URL, 'nucleotide', 'FOO', 0.1, 5, 0.9);
+
+        expect(result).toEqual(['A2C']);
+    })
+})
