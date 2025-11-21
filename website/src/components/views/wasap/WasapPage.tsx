534d27ee3fc6fbed67bd67fdee1db1d14638cbca
Felix Hennig <mail@felixhennig.com>
2025-11-21 16:12:42 +0100

    refactor(website): create custom hook

--- website/src/components/views/wasap/WasapPage.tsx
+++ website/src/components/views/wasap/WasapPage.tsx
@@ -1,12 +1,11 @@
-import type { CustomColumn, MeanProportionInterval } from '@genspectrum/dashboard-components/util';
+import type { MeanProportionInterval } from '@genspectrum/dashboard-components/util';
 import { useQuery } from '@tanstack/react-query';
 import { useMemo } from 'react';
 import { type FC } from 'react';
 
-import { RESISTANCE_MUTATIONS, resistanceMutationAnnotations } from './resistanceMutations';
-import { getCladeLineages } from '../../../lapis/getCladeLineages';
+import { resistanceMutationAnnotations } from './resistanceMutations';
+import { useWasapPageData } from './useWasapPageData';
 import { getDateRange } from '../../../lapis/getDateRange';
-import { getMutations, getMutationsForVariant } from '../../../lapis/getMutations';
 import { getTotalCount } from '../../../lapis/getTotalCount';
 import { wastewaterConfig } from '../../../types/wastewaterConfig';
 import { Loading } from '../../../util/Loading';
@@ -31,16 +30,7 @@ export const WasapPageInner: FC<WasapPageProps> = ({ currentUrl }) => {
     );
 
     // fetch which mutations should be analyzed
-    const {
-        data: mutationSelection,
-        isPending,
-        isError,
-    } = useQuery({
-        queryKey: [base, analysis],
-        queryFn: () => fetchMutationSelection(analysis),
-    });
-
-    const [displayMutations, customColumns] = getDisplayMutationsAndCustomColumns(mutationSelection);
+    const { displayMutations, customColumns, isPending, isError } = useWasapPageData(analysis);
 
     let initialMeanProportionInterval: MeanProportionInterval = { min: 0.0, max: 1.0 };
     if (analysis.mode === 'manual' && analysis.mutations === undefined) {
@@ -177,106 +167,3 @@ const WasapStats = () => (
         <DateRange />
     </div>
 );
-
-type AllMutations = {
-    type: 'all';
-};
-
-type SelectedMutations = {
-    type: 'selected';
-    mutations: string[];
-};
-
-type SelectedWithJaccard = {
-    type: 'jaccard';
-    mutationsWithScore: { mutation: string; jaccardIndex: number }[];
-};
-
-type MutationSelection = AllMutations | SelectedMutations | SelectedWithJaccard;
-
-/**
- * Takes the analysis settings and then returns a list of mutations that should be analysed,
- * based on the settings. can also return the string 'all', which means that everything should
- * be analysed, no specific selection.
- *
- * For some modes, additional data will be fetched to decide which mutations to analyse.
- */
-async function fetchMutationSelection(analysis: WasapAnalysisFilter): Promise<MutationSelection> {
-    switch (analysis.mode) {
-        case 'manual':
-            return analysis.mutations ? { type: 'selected', mutations: analysis.mutations } : { type: 'all' };
-        case 'variant':
-            if (!analysis.variant) {
-                return { type: 'selected', mutations: [] };
-            }
-            return getMutationsForVariant(
-                wastewaterConfig.wasap.covSpectrum.lapisBaseUrl,
-                analysis.sequenceType,
-                analysis.variant,
-                analysis.minProportion,
-                analysis.minCount,
-                analysis.minJaccard,
-            ).then((r) => ({ type: 'jaccard', mutationsWithScore: r }));
-        case 'resistance':
-            return { type: 'selected', mutations: RESISTANCE_MUTATIONS[analysis.resistanceSet] };
-        case 'untracked': {
-            const variantsToExclude =
-                analysis.excludeSet === 'custom'
-                    ? analysis.excludeVariants
-                    : await getCladeLineages(
-                          wastewaterConfig.wasap.covSpectrum.lapisBaseUrl,
-                          wastewaterConfig.wasap.covSpectrum.cladeField,
-                          wastewaterConfig.wasap.covSpectrum.lineageField,
-                          true,
-                      ).then((r) => Object.values(r));
-            if (variantsToExclude === undefined) {
-                return { type: 'selected', mutations: [] };
-            }
-            const [excludeMutations, allMuts] = await Promise.all([
-                Promise.all(
-                    variantsToExclude.map((v) =>
-                        getMutations(wastewaterConfig.wasap.covSpectrum.lapisBaseUrl, analysis.sequenceType, v, 0.8, 9),
-                    ),
-                ).then((r) => r.flat()),
-                getMutations(wastewaterConfig.wasap.lapisBaseUrl, analysis.sequenceType, undefined, 0.05, 5),
-            ]);
-            return {
-                type: 'selected',
-                mutations: allMuts.filter((m) => !excludeMutations.includes(m)),
-            };
-        }
-    }
-}
-
-function getDisplayMutationsAndCustomColumns(
-    mutationSelection: MutationSelection | undefined,
-): [string[] | undefined, CustomColumn[] | undefined] {
-    let displayMutations: string[] | undefined = undefined;
-    let customColumns: CustomColumn[] | undefined = undefined;
-    if (mutationSelection !== undefined) {
-        switch (mutationSelection.type) {
-            case 'all':
-                break; // keep it as undefined
-            case 'selected': {
-                displayMutations = mutationSelection.mutations;
-                break;
-            }
-            case 'jaccard': {
-                displayMutations = mutationSelection.mutationsWithScore.map(({ mutation }) => mutation);
-                customColumns = [
-                    {
-                        header: 'Jaccard index',
-                        values: Object.fromEntries(
-                            mutationSelection.mutationsWithScore.map(({ mutation, jaccardIndex }) => [
-                                mutation,
-                                jaccardIndex.toPrecision(2),
-                            ]),
-                        ),
-                    },
-                ];
-                break;
-            }
-        }
-    }
-    return [displayMutations, customColumns];
-}
