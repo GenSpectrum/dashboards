f40919f15f6703a6e9b668e36c4dda6a2964f6ed
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2026-01-20 09:23:44 +0100

    995 refactor: make try block properly localized

--- website/src/lapis/getLastUpdatedDate.ts
+++ website/src/lapis/getLastUpdatedDate.ts
@@ -15,25 +15,27 @@ const logger = getClientLogger('getLastUpdatedDate');
 setupDayjs();
 
 export async function getLastUpdatedDate(lapisUrl: string) {
+    let response;
     try {
-        const response = await axios.get(`${lapisUrl}/sample/info`);
-        const parsedLapisInfo = lapisInfoSchema.safeParse(response.data);
-
-        if (parsedLapisInfo.success) {
-            const timestamp = Number(parsedLapisInfo.data.dataVersion);
-            if (isNaN(timestamp)) {
-                logger.error(`Got invalid timestamp from lapis info: '${parsedLapisInfo.data.dataVersion}'`);
-                return null;
-            }
-            return dayjs.unix(timestamp).locale('de').calendar(null, { sameElse: '[on] YYYY-MM-DD' });
-        }
-
-        logger.error(
-            `Failed to parse lapis info: ${JSON.stringify(parsedLapisInfo)} (was ${JSON.stringify(response.data)})`,
-        );
-        return null;
+        response = await axios.get(`${lapisUrl}/sample/info`);
     } catch (error) {
-        logger.error(`Failed to fetch lapis info: ${JSON.stringify(error)}`);
+        logger.error(`Failed to fetch lapis info: ${(error as Error).toString()}`);
         return null;
     }
+
+    const parsedLapisInfo = lapisInfoSchema.safeParse(response.data);
+
+    if (parsedLapisInfo.success) {
+        const timestamp = Number(parsedLapisInfo.data.dataVersion);
+        if (isNaN(timestamp)) {
+            logger.error(`Got invalid timestamp from lapis info: '${parsedLapisInfo.data.dataVersion}'`);
+            return null;
+        }
+        return dayjs.unix(timestamp).locale('de').calendar(null, { sameElse: '[on] YYYY-MM-DD' });
+    }
+
+    logger.error(
+        `Failed to parse lapis info: ${JSON.stringify(parsedLapisInfo)} (was ${JSON.stringify(response.data)})`,
+    );
+    return null;
 }
