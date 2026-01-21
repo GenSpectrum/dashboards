fec1f3956f63072819b83bd57fd859bdcfd5efce
Fabian Engelniederhammer <92720311+fengelniederhammer@users.noreply.github.com>
2026-01-21 08:41:15 +0100

    fix(website): register dayjs plugin to fix "last updated" display (#997)
    * 995 fix: register calendar plugin in dayjs
    
    * 995 refactor: make try block properly localized

--- website/src/lapis/getLastUpdatedDate.ts
+++ website/src/lapis/getLastUpdatedDate.ts
@@ -3,6 +3,7 @@ import dayjs from 'dayjs';
 import { z } from 'zod';
 
 import { getClientLogger } from '../clientLogger.ts';
+import setupDayjs from '../util/setupDayjs.ts';
 
 const lapisInfoSchema = z.object({
     dataVersion: z.string(),
@@ -11,26 +12,30 @@ export type LapisInfo = z.infer<typeof lapisInfoSchema>;
 
 const logger = getClientLogger('getLastUpdatedDate');
 
+setupDayjs();
+
 export async function getLastUpdatedDate(lapisUrl: string) {
+    let response;
     try {
-        const response = await axios.get(`${lapisUrl}/sample/info`);
-        const parsedLapisInfo = lapisInfoSchema.safeParse(response.data);
-
-        if (parsedLapisInfo.success) {
-            const timestamp = Number(parsedLapisInfo.data.dataVersion);
-            if (isNaN(timestamp)) {
-                logger.error(`Got invalid timestamp from lapis info: '${parsedLapisInfo.data.dataVersion}'`);
-                return null;
-            }
-            return dayjs.unix(timestamp).locale('de').calendar(null, { sameElse: '[on] YYYY-MM-DD' });
-        }
-
-        logger.error(
-            `Failed to parse lapis info: ${JSON.stringify(parsedLapisInfo)} (was ${JSON.stringify(response.data)})`,
-        );
-        return null;
+        response = await axios.get(`${lapisUrl}/sample/info`);
     } catch (error) {
-        logger.error(`Failed to fetch lapis info: ${JSON.stringify(error)}`);
+        logger.error(`Failed to fetch lapis info: ${(error as Error).toString()}`);
         return null;
     }
+
+    const parsedLapisInfo = lapisInfoSchema.safeParse(response.data);
+
+    if (parsedLapisInfo.success) {
+        const timestamp = Number(parsedLapisInfo.data.dataVersion);
+        if (isNaN(timestamp)) {
+            logger.error(`Got invalid timestamp from lapis info: '${parsedLapisInfo.data.dataVersion}'`);
+            return null;
+        }
+        return dayjs.unix(timestamp).locale('de').calendar(null, { sameElse: '[on] YYYY-MM-DD' });
+    }
+
+    logger.error(
+        `Failed to parse lapis info: ${JSON.stringify(parsedLapisInfo)} (was ${JSON.stringify(response.data)})`,
+    );
+    return null;
 }
