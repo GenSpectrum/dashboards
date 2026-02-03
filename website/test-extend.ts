6375fde0e6cf48d8bc149abc843f9d33a3d23764
Felix Hennig <mail@felixhennig.com>
2026-02-03 16:34:42 +0100

    add test

--- website/test-extend.ts
+++ website/test-extend.ts
@@ -2,7 +2,7 @@ import { setupWorker } from 'msw/browser';
 import '@testing-library/jest-dom/vitest';
 import { it as itBase } from 'vitest';
 
-import { AstroApiRouteMocker, BackendRouteMocker, LapisRouteMocker } from './routeMocker.ts';
+import { AstroApiRouteMocker, BackendRouteMocker, CovSpectrumRouteMocker, LapisRouteMocker } from './routeMocker.ts';
 import setupDayjs from './src/util/setupDayjs.ts';
 
 setupDayjs();
@@ -12,6 +12,7 @@ export const worker = setupWorker();
 export const lapisRouteMocker = new LapisRouteMocker(worker);
 export const astroApiRouteMocker = new AstroApiRouteMocker(worker);
 export const backendRouteMocker = new BackendRouteMocker(worker);
+export const covSpectrumRouteMocker = new CovSpectrumRouteMocker(worker);
 
 /**
  * Test extension to access the mocks. Import it:
@@ -28,14 +29,24 @@ export const backendRouteMocker = new BackendRouteMocker(worker);
  *         ...
  */
 export const it = itBase.extend<{
-    routeMockers: { lapis: LapisRouteMocker; astro: AstroApiRouteMocker; backend: BackendRouteMocker };
+    routeMockers: {
+        lapis: LapisRouteMocker;
+        astro: AstroApiRouteMocker;
+        backend: BackendRouteMocker;
+        covSpectrum: CovSpectrumRouteMocker;
+    };
 }>({
     routeMockers: [
         // eslint-disable-next-line no-empty-pattern -- vitest needs the 1st arg to be an object destructor
         async ({}, use) => {
             await worker.start({ onUnhandledRequest: 'error' });
 
-            await use({ lapis: lapisRouteMocker, astro: astroApiRouteMocker, backend: backendRouteMocker });
+            await use({
+                lapis: lapisRouteMocker,
+                astro: astroApiRouteMocker,
+                backend: backendRouteMocker,
+                covSpectrum: covSpectrumRouteMocker,
+            });
 
             worker.resetHandlers();
             worker.stop();
