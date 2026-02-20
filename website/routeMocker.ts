43ae3c406f80036b98050d178a062a19ef318995
Felix Hennig <mail@felixhennig.com>
2026-02-20 12:44:58 +0100

    specify type; format

--- website/routeMocker.ts
+++ website/routeMocker.ts
@@ -7,6 +7,7 @@ import { expect } from 'vitest';
 import { type OrganismsConfig } from './src/config';
 import type { CollectionRaw } from './src/covspectrum/types.ts';
 import type { LapisInfo } from './src/lapis/getLastUpdatedDate.ts';
+import type { ParsedQueryResult } from './src/lapis/parseQuery.ts';
 import type { ProblemDetail } from './src/types/ProblemDetail.ts';
 import type {
     SubscriptionPutRequest,
@@ -145,11 +146,7 @@ export class LapisRouteMocker {
         this.workerOrServer.use(http.post(`${DUMMY_LAPIS_URL}/sample/aminoAcidMutations`, resolver(cases)));
     }
 
-    mockPostQueryParse(
-        body: { queries: string[] },
-        response: { data: unknown[] },
-        statusCode = 200,
-    ) {
+    mockPostQueryParse(body: { queries: string[] }, response: { data: ParsedQueryResult[] }, statusCode = 200) {
         this.workerOrServer.use(
             http.post(`${DUMMY_LAPIS_URL}/query/parse`, resolver([{ statusCode, body, response }])),
         );
