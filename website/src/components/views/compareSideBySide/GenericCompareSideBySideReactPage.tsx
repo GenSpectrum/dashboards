876db2167a6a285785af9030b278ffa7f876bf10
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2026-02-27 09:31:04 +0100

    1038 move add column button

--- website/src/components/views/compareSideBySide/GenericCompareSideBySideReactPage.tsx
+++ website/src/components/views/compareSideBySide/GenericCompareSideBySideReactPage.tsx
@@ -47,80 +47,86 @@ export const GenericCompareSideBySideReactPage: FC<GenericCompareSideBySideReact
             view={view}
             downloadLinks={downloadLinks}
         >
-            <div className='flex flex-col gap-4'>
-                {/* FILTERS SECTION - Flexbox horizontal scroll */}
-                <div className='flex overflow-x-auto border-b-2 border-gray-200'>
-                    {columnsArray.map(([id, datasetAndVariantData]) => {
-                        return (
-                            <div
-                                key={id}
-                                className='flex min-w-[500px] flex-1 flex-col gap-4 border-r-2 border-gray-200 px-2 pb-4'
-                            >
-                                {pageState.filters.size > 1 && (
-                                    <a
-                                        className='block w-full px-2 py-1 text-sm font-light hover:bg-neutral-100'
-                                        href={view.pageStateHandler.toUrl(
-                                            view.pageStateHandler.removeFilter(pageState, id),
-                                        )}
-                                    >
-                                        Remove column
-                                    </a>
-                                )}
-                                <CompareSideBySidePageStateSelector
-                                    view={view}
-                                    filterId={id}
-                                    draftPageState={draftPageState}
-                                    setDraftPageState={setDraftPageState}
-                                    enableAdvancedQueryFilter={isStaging}
-                                />
-                            </div>
-                        );
-                    })}
-                    <a
-                        className='py-4 pt-8 text-left text-sm font-light hover:bg-neutral-100'
-                        href={view.pageStateHandler.toUrl(view.pageStateHandler.addEmptyFilter(pageState))}
-                        style={{ writingMode: 'vertical-rl' }}
-                    >
-                        Add column
-                    </a>
-                </div>
+            {/* Main horizontal layout with columns and add button */}
+            <div className='flex'>
+                {/* Columns container */}
+                <div className='flex flex-1 flex-col gap-4'>
+                    {/* FILTERS SECTION - Flexbox horizontal scroll */}
+                    <div className='flex overflow-x-auto border-b-2 border-gray-200'>
+                        {columnsArray.map(([id, datasetAndVariantData]) => {
+                            return (
+                                <div
+                                    key={id}
+                                    className='flex min-w-[500px] flex-1 flex-col gap-4 border-r-2 border-gray-200 px-2 pb-4'
+                                >
+                                    {pageState.filters.size > 1 && (
+                                        <a
+                                            className='block w-full px-2 py-1 text-sm font-light hover:bg-neutral-100'
+                                            href={view.pageStateHandler.toUrl(
+                                                view.pageStateHandler.removeFilter(pageState, id),
+                                            )}
+                                        >
+                                            Remove column
+                                        </a>
+                                    )}
+                                    <CompareSideBySidePageStateSelector
+                                        view={view}
+                                        filterId={id}
+                                        draftPageState={draftPageState}
+                                        setDraftPageState={setDraftPageState}
+                                        enableAdvancedQueryFilter={isStaging}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </div>
 
-                {/* APPLY BUTTON SECTION */}
-                <div className='border-b-2 border-gray-200 bg-gray-50 p-4'>
-                    <div className='flex justify-center'>
-                        <ApplyFilterButton
-                            pageStateHandler={view.pageStateHandler}
-                            newPageState={draftPageState}
-                            setPageState={setPageState}
-                            className='min-w-[200px]'
-                        />
+                    {/* APPLY BUTTON SECTION */}
+                    <div className='border-b-2 border-gray-200 bg-gray-50 p-4'>
+                        <div className='flex justify-center'>
+                            <ApplyFilterButton
+                                pageStateHandler={view.pageStateHandler}
+                                newPageState={draftPageState}
+                                setPageState={setPageState}
+                                className='min-w-[200px]'
+                            />
+                        </div>
+                    </div>
+
+                    {/* DATA SECTION - CSS Grid for alignment */}
+                    <div
+                        className='grid overflow-x-auto gap-y-4'
+                        style={{
+                            gridTemplateColumns: `repeat(${columnCount}, minmax(500px, 1fr))`,
+                            gridAutoRows: 'auto',
+                        }}
+                    >
+                        {columnsArray.map(([id, datasetAndVariantData], colIndex) => {
+                            return (
+                                <div
+                                    key={id}
+                                    className='contents'
+                                >
+                                    <GenericCompareSideBySideDataDisplay
+                                        view={view}
+                                        datasetAndVariantData={datasetAndVariantData}
+                                        hideMutationComponents={hideMutationComponents}
+                                        columnIndex={colIndex}
+                                    />
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
 
-                {/* DATA SECTION - CSS Grid for alignment */}
-                <div
-                    className='grid overflow-x-auto gap-y-4'
-                    style={{
-                        gridTemplateColumns: `repeat(${columnCount}, minmax(500px, 1fr))`,
-                        gridAutoRows: 'auto',
-                    }}
+                {/* ADD COLUMN BUTTON - spans full height */}
+                <a
+                    className='flex items-center border-l-2 border-gray-200 px-2 py-4 text-left text-sm font-light hover:bg-neutral-100'
+                    href={view.pageStateHandler.toUrl(view.pageStateHandler.addEmptyFilter(pageState))}
+                    style={{ writingMode: 'vertical-rl' }}
                 >
-                    {columnsArray.map(([id, datasetAndVariantData], colIndex) => {
-                        return (
-                            <div
-                                key={id}
-                                className='contents'
-                            >
-                                <GenericCompareSideBySideDataDisplay
-                                    view={view}
-                                    datasetAndVariantData={datasetAndVariantData}
-                                    hideMutationComponents={hideMutationComponents}
-                                    columnIndex={colIndex}
-                                />
-                            </div>
-                        );
-                    })}
-                </div>
+                    Add column
+                </a>
             </div>
         </OrganismViewPageLayout>
     );
