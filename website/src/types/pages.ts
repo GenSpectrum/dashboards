197151f5781e1576b4f2fb65f7db5c8ce0f884c1
Felix Hennig <mail@felixhennig.com>
2026-03-16 11:55:56 +0100

    Add collection page stubs

--- website/src/types/pages.ts
+++ website/src/types/pages.ts
@@ -2,4 +2,9 @@ export const Page = {
     createSubscription: '/subscriptions/create',
     subscriptionsOverview: '/subscriptions',
     dataSources: '/data',
+    collectionsOverview: '/collections',
+    collectionsForOrganism: (organism: string) => `/collections/${organism}`,
+    createCollection: (organism: string) => `/collections/${organism}/create`,
+    viewCollection: (organism: string, id: string) => `/collections/${organism}/${id}`,
+    editCollection: (organism: string, id: string) => `/collections/${organism}/${id}/edit`,
 } as const;
