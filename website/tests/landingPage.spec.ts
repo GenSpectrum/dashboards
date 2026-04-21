273f114234c71185d96605bc65adac73e391ae42
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2026-04-21 10:36:34 +0200

    1091 check that genes are there

--- website/tests/landingPage.spec.ts
+++ website/tests/landingPage.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from '@playwright/test';
 
 import { test } from './e2e.fixture.ts';
+import { organismOptions } from './helpers.ts';
 import { allOrganisms, organismConfig } from '../src/types/Organism.ts';
 
 const organismsWithGenome = allOrganisms.filter((organism) => 'genome' in organismConfig[organism]);
@@ -12,6 +13,12 @@ test.describe('Organism landing pages', () => {
 
             await expect(landingPage.organismHeading(organism)).toBeVisible();
             await expect(landingPage.genomeViewerHeading).toBeVisible();
+
+            const options = organismOptions[organism];
+            if ('gene' in options) {
+                await expect(landingPage.geneLabel(options.gene)).toBeVisible();
+            }
+
             await landingPage.expectToSeeNoComponentErrors();
         });
     }
