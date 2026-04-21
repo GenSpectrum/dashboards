376b393841db7f6687f6a58f0c03f6f12f16c5ff
Fabian Engelniederhammer <92720311+fengelniederhammer@users.noreply.github.com>
2026-04-21 09:28:16 +0200

    test(website): add e2e test for single variant page (#1146)
    * test(website): add e2e test for single variant page
    
    * 1089 format
    
    * 1089 choose the correct lineage option (not the one in the collections thingy)

---
+++ website/tests/SingleVariantPage.ts
@@ -0,0 +1,32 @@
+import { expect, type Page } from '@playwright/test';
+
+import { ViewPage } from './ViewPage.ts';
+import { paths } from '../src/types/Organism.ts';
+import { type OrganismWithViewKey } from '../src/views/routing';
+import { singleVariantViewKey } from '../src/views/viewKeys';
+
+export class SingleVariantPage extends ViewPage {
+    public readonly mutationField;
+
+    constructor(page: Page) {
+        super(page);
+        this.mutationField = this.page.getByRole('combobox', { name: 'Enter a mutation', exact: false });
+    }
+
+    public async goto(organism: OrganismWithViewKey<typeof singleVariantViewKey>) {
+        await this.page.goto(`${paths[organism].basePath}/single-variant`);
+    }
+
+    public async selectVariant(options: { lineage?: string; lineageFieldPlaceholder?: string; mutation?: string }) {
+        if (options.lineage !== undefined && options.lineageFieldPlaceholder !== undefined) {
+            const { lineage, lineageFieldPlaceholder } = options;
+            const lineageFieldLocator = this.page.getByPlaceholder(lineageFieldPlaceholder);
+            await expect(lineageFieldLocator).toBeVisible();
+            await this.fillLineageField(lineageFieldLocator, lineage);
+        }
+
+        if (options.mutation !== undefined) {
+            await this.fillMutationField(this.mutationField, options.mutation);
+        }
+    }
+}
